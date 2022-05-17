217f6578f22ddb0718766f90be606e0d3b745732
Gliperal <ananasviolet@fintmcwells.com>
2022-05-17 16:27:10 -0700

    Pull in UserPreferences file

--- BirdStudioRefactor/CustomCommands.cs
+++ BirdStudioRefactor/CustomCommands.cs
@@ -1,27 +1,51 @@
 ﻿using System.Windows.Input;
 
-namespace BirdStudio.Commands
+namespace BirdStudioRefactor.Commands
 {
     public class CustomCommands
     {
-        public static RoutedUICommand Undo = new RoutedUICommand(
+        private static RoutedUICommand makeCommand(string text, string name, KeyGesture defaultInput)
+        {
+            InputGestureCollection inputGesture = null;
+            KeyGesture input = UserPreferences.getKeyBinding(name, defaultInput);
+            if (input != null)
+                inputGesture = new InputGestureCollection() { input };
+            return new RoutedUICommand(
+                text,
+                name,
+                typeof(CustomCommands),
+                inputGesture
+            );
+        }
+
+        public static RoutedUICommand Undo = makeCommand(
             "Undo",
             "Undo",
-            typeof(CustomCommands),
-            new InputGestureCollection()
-            {
-                new KeyGesture(Key.Z, ModifierKeys.Control)
-            }
+            new KeyGesture(Key.Z, ModifierKeys.Control)
         );
 
-        public static RoutedUICommand Redo = new RoutedUICommand(
+        public static RoutedUICommand Redo = makeCommand(
             "Redo",
             "Redo",
-            typeof(CustomCommands),
-            new InputGestureCollection()
-            {
-                new KeyGesture(Key.Y, ModifierKeys.Control)
-            }
+            new KeyGesture(Key.Y, ModifierKeys.Control)
+        );
+
+        public static RoutedUICommand NewBranch = makeCommand(
+            "New Branch",
+            "NewBranch",
+            null
+        );
+
+        public static RoutedUICommand AddBranch = makeCommand(
+            "Add Branch",
+            "AddBranch",
+            null
+        );
+
+        public static RoutedUICommand RemoveBranch = makeCommand(
+            "Remove Branch",
+            "RemoveBranch",
+            null
         );
     }
 }
